2c272a5ac57e10c2da8cb66b9cd2a6d0345f6f63
Sagar Vemuri <sagar.vemuri@gmail.com>
2015-05-05 23:51:49 -0700

    Add closure code to perform the operations. Add square root functionality.

--- iCalc/ViewController.swift
+++ iCalc/ViewController.swift
@@ -25,6 +25,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation1 { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation1(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
     
     var operandStack = Array<Double>()
