e09c650e50fc1cc3e579852b2f25ca9891bf4a1a
Sagar Vemuri <sagar.vemuri@gmail.com>
2015-05-07 11:28:11 -0700

    Add a CalculatorBrain class.

---
+++ iCalc/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  iCalc
+//
+//  Created by Sagar Vemuri on 5/7/15.
+//  Copyright (c) 2015 Sagar Vemuri. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    var opStack = [Op]()
+    
+    var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
+}
